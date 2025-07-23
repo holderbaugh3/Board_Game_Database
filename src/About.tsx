2ebc4bc44150f2948433524302829cfeed0ade82
holderbaugh3 <holderbaugh3@gmail.com>
2025-07-23 12:22:01 -0500

    Updated about section

--- src/About.tsx
+++ src/About.tsx
@@ -11,7 +11,10 @@ class About extends React.Component {
             <div style={divStyle}>
                 <h2>About Page</h2>
                 <main>
-                    <p>This section contains information about...</p>
+                    <p>Jana Holderbaugh graduated from Michigan State in 2020. From there Jana
+                        has been in two major positions one at Yahoo as a Front end developer,
+                        and the other at Amazon as a Full stack developer.
+                    </p>
                 </main>
             </div>
         )
